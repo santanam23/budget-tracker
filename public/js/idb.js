a4b400820a7ce231ebb5e23a2ba1e9c820963166
santanam23 <mirmir11.ms@gmail.com>
2022-03-31 20:29:45 -0500

    added idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -26,4 +26,59 @@ request.onsuccess = function(event) {
     // log error here
     console.log(event.target.errorCode);
   };
+// This function will be executed if we attempt to submit a new budget and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_budget'], 'readwrite');
   
+    // access the object store for `new_pizza`
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+  }  
+  function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/budget', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          // access the new_pizza object store
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved budget has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
+  }
+  // listen for app coming back online
+window.addEventListener('online', uploadBudget);
