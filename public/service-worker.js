caa24552c8fcf32069f9ee7ae6d3e7080d0b1469
santanam23 <mirmir11.ms@gmail.com>
2022-03-31 21:09:24 -0500

    unregister service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,17 +5,17 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/public/css/style.css',
-  '/public/js/index.js',
-  '/public/js/idb.js',
-  '/public/icons/icon-72x72.png',
-  '/public/icons/icon-96x96.png',
-  '/public/icons/icon-128x128.png',
-  '/public/icons/icon-144x144.png',
-  '/public/icons/icon-152x152.pgn',
-  '/public/icons/icon-192x192.pgn',
-  '/public/icons/icon-384x384.pgn',
-  '/public/icons/icon-512x512.pgn'
+  '/css/style.css',
+  '/js/index.js',
+  '/js/idb.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.pgn',
+  '/icons/icon-192x192.pgn',
+  '/icons/icon-384x384.pgn',
+  '/icons/icon-512x512.pgn'
 ];
 
 // Install the service worker
--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 // routes
-app.use(require("./routes"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
